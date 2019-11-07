475e75cbafe049e1088c6451c142e5b0fef83c80
Boryana Lengerova <b.arizanova@abv.bg>
2019-11-07 13:12:23 +0200

    Add

--- 04. Code-First/2. Hospital Database Modification/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P01_HospitalDatabase.Data
-{
-    public class Configuration
-    {
-        public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=Hospital;Integrated Security=True;";
-    }
-}
